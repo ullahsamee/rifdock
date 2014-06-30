37e8f57f885689aa315b4f5a96438450ac2e2c7a
Will Sheffler <willsheffler@gmail.com>
2014-06-30 16:10:49 -0700

    mol TypeHolders now use mol::iterators

--- src/util/mpl/TypesHolders.hh
+++ src/util/mpl/TypesHolders.hh
@@ -1,13 +1,13 @@
 #ifndef INCLUDED_util_mpl_TypesHolders_HH
 #define INCLUDED_util_mpl_TypesHolders_HH
 
-#include <boost/mpl/empty.hpp>
-#include <boost/mpl/pop_front.hpp>
-#include <boost/mpl/front.hpp>
+#include <boost/mpl/deref.hpp>
 #include <boost/mpl/assert.hpp>
 #include <boost/mpl/is_sequence.hpp>
+#include <boost/mpl/begin.hpp>
+#include <boost/mpl/end.hpp>
+#include <boost/mpl/next.hpp>
 #include <vector>
-#include <functional>
 
 namespace scheme {
 namespace util {
@@ -17,21 +17,13 @@ namespace mpl {
 template<typename T> struct type2type { typedef T type; };
 
 namespace types_holder_details {
-    template<
-        typename Types,
-        bool Empty = false
-    >
+    using namespace boost::mpl;
+    template< typename Beg, typename End >
     struct TypesValuesBase :
-        public TypesValuesBase<
-                    typename boost::mpl::pop_front<Types>::type,
-                    boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value
-               >
+        public TypesValuesBase<typename next<Beg>::type, End >
     {
-        typedef TypesValuesBase<
-                     typename boost::mpl::pop_front<Types>::type,
-                     boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value
-                > Super;
-        typedef typename boost::mpl::front<Types>::type Held_t;
+        typedef TypesValuesBase< typename next<Beg>::type, End > Super;
+        typedef typename boost::mpl::deref<Beg>::type Held_t;
     
         using Super::get_impl;
         Held_t val_;
@@ -39,8 +31,8 @@ namespace types_holder_details {
         Held_t       & get_impl(type2type<Held_t>)       { return val_; }
         Held_t const & get_impl(type2type<Held_t>) const { return val_; }
     };
-    template<typename Types>
-    struct TypesValuesBase<Types,true> {
+    template<typename End>
+    struct TypesValuesBase<End,End> {
         void get_impl(){}
     };
 }
@@ -49,34 +41,26 @@ namespace types_holder_details {
 ///@tparam Types a "list" of types to hold instances of, most boost::mpl type containers work
 ///@detail
 template< typename Types >
-struct TypesValues :
-    public types_holder_details::TypesValuesBase< Types, boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value >
+struct TypesValues : public types_holder_details::TypesValuesBase< 
+                                typename boost::mpl::begin<Types>::type ,
+                                typename boost::mpl::end  <Types>::type >
 {
     BOOST_MPL_ASSERT(( boost::mpl::is_sequence<Types> ));
-    template<typename T> T       & get()       { return get_impl( type2type<T>() ); }
-    template<typename T> T const & get() const { return get_impl( type2type<T>() ); }
+    template<typename T> T       & get()       { return this->get_impl( type2type<T>() ); }
+    template<typename T> T const & get() const { return this->get_impl( type2type<T>() ); }
 };
 
 namespace types_container_details {
+    using namespace boost::mpl;
     template<
-        typename Types,
+        typename Beg, typename End,
         template<typename T,typename Alloc> class Container = std::vector,
-        template<typename T> class Alloc = std::allocator,
-        bool Empty = false
+        template<typename T> class Alloc = std::allocator
     >
-    struct TypesContainersBase :
-        public TypesContainersBase<
-                    typename boost::mpl::pop_front<Types>::type,
-                    Container, Alloc,
-                    boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value
-               >
+    struct TypesContainersBase : public TypesContainersBase< typename next<Beg>::type, End, Container, Alloc >
     {
-        typedef TypesContainersBase<
-                     typename boost::mpl::pop_front<Types>::type,
-                     Container, Alloc,
-                     boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value
-                > Super;
-        typedef typename boost::mpl::front<Types>::type Held_t;
+        typedef TypesContainersBase< typename next<Beg>::type, End , Container, Alloc > Super;
+        typedef typename deref<Beg>::type Held_t;
         typedef Container<Held_t,Alloc<Held_t> > Container_t;
     
         using Super::get_impl;
@@ -86,11 +70,11 @@ namespace types_container_details {
         Container_t const & get_impl(type2type<Held_t>) const { return val_; }
     };
     template<
-        typename Types,
+        typename End,
         template<typename T,typename Alloc> class Container,
         template<typename T> class Alloc
     >
-    struct TypesContainersBase<Types,Container,Alloc,true> {
+    struct TypesContainersBase<End,End,Container,Alloc> {
         void get_impl(){}
     };
 }
@@ -103,126 +87,16 @@ template<
     template<typename T,typename Alloc> class Container = std::vector,
     template<typename T> class Alloc = std::allocator
 >
-struct TypesContainers :
-    public types_container_details::TypesContainersBase< Types, Container, Alloc, boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value >
+struct TypesContainers : public types_container_details::TypesContainersBase<
+                                        typename boost::mpl::begin<Types>::type ,
+                                        typename boost::mpl::end  <Types>::type ,
+                                        Container, Alloc >
 {
     BOOST_MPL_ASSERT(( boost::mpl::is_sequence<Types> ));
-    template<typename T> Container<T,Alloc<T> >       & get()       { return get_impl( type2type<T>() ); }
-    template<typename T> Container<T,Alloc<T> > const & get() const { return get_impl( type2type<T>() ); }
+    template<typename T> Container<T,Alloc<T> >       & get()       { return this->get_impl( type2type<T>() ); }
+    template<typename T> Container<T,Alloc<T> > const & get() const { return this->get_impl( type2type<T>() ); }
 };
 
-
-namespace types_container_details {
-    template<
-        typename Types,
-        template<typename T, typename Compare, typename Alloc> class Container = std::vector,
-        template<typename T> class Compare = std::less,
-        template<typename T> class Alloc = std::allocator,        
-        bool Empty = false
-    >
-    struct TypesContainersCompBase :
-        public TypesContainersCompBase<
-                    typename boost::mpl::pop_front<Types>::type,
-                    Container, Compare, Alloc, 
-                    boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value
-               >
-    {
-        typedef TypesContainersCompBase<
-                     typename boost::mpl::pop_front<Types>::type,
-                     Container, Compare, Alloc,
-                     boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value
-                > Super;
-        typedef typename boost::mpl::front<Types>::type Held_t;
-        typedef Container<Held_t,Compare<Held_t>,Alloc<Held_t> > Container_t;
-    
-        using Super::get_impl;
-        Container_t val_;
-    
-        Container_t       & get_impl(type2type<Held_t>)       { return val_; }
-        Container_t const & get_impl(type2type<Held_t>) const { return val_; }
-    };
-    template<
-        typename Types,
-        template<typename T, typename Compare, typename Alloc> class Container,
-        template<typename T> class Compare,
-        template<typename T> class Alloc        
-    >
-    struct TypesContainersCompBase<Types,Container,Compare,Alloc,true> {
-        void get_impl(){}
-    };
-}
-
-///@brief holds Containers of a list of Types and provides a generic accessor get<T>()
-///@tparam Types a "list" of types to hold instances of, most boost::mpl type containers work
-///@detail
-template<
-    typename Types,
-    template<typename T, typename Compare, typename Alloc> class Container = std::vector,
-    template<typename T> class Compare = std::less,
-    template<typename T> class Alloc = std::allocator
->
-struct TypesContainersComp :
-    public types_container_details::TypesContainersCompBase< Types, Container, Compare, Alloc, boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value >
-{
-    BOOST_MPL_ASSERT(( boost::mpl::is_sequence<Types> ));     
-    template<typename T> Container<T,Compare<T>,Alloc<T> >       & get()       { return get_impl( type2type<T>() ); }
-    template<typename T> Container<T,Compare<T>,Alloc<T> > const & get() const { return get_impl( type2type<T>() ); }
-};
-
-
-
-// namespace types_svec_details {
-//     template<
-//         typename Types,
-//         bool Empty = false
-//     >
-//     struct TypesVecSBase :
-//         public TypesVecSBase<
-//                     typename boost::mpl::pop_front<Types>::type,
-//                     boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value
-//                >
-//     {
-//         typedef TypesVecSBase<
-//                      typename boost::mpl::pop_front<Types>::type,
-//                      boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value
-//                 > Super;
-//         typedef typename boost::mpl::front<Types>::type Held_t;
-//         typedef std::vector<Held_t> Container_t;
-//         typedef std::pair<typename Container_t::iterator,typename Container_t::iterator> IterPair;
-//         typedef std::pair<typename Container_t::const_iterator,typename Container_t::const_iterator> ConstIterPair;
-
-//         using Super::get_impl;
-//         using Super::add_impl;
-//         using Super::size_impl;    
-
-//         Container_t vals_;
-
-//         IterPair      get_impl(type2type<Held_t>)       { return make_pair(vals_.begin(),vals_.end()); }
-//         ConstIterPair get_impl(type2type<Held_t>) const { return make_pair(vals_.begin(),vals_.end()); }
-
-//         void add_impl( Held_t const & val ) { vals_.push_back(val); }
-
-//         size_t size_impl( type2type<Held_t> ) { return vals_.size(); }
-//     };
-//     template<typename Types>
-//     struct TypesVecSBase<Types,true> {
-//         void get_impl(){}
-//         void add_impl(){}    
-//         void size_impl(){}    
-//     };
-// }
-
-// template< typename Types >
-// struct TypesVecS :
-//     public types_svec_details::TypesVecSBase< Types, boost::mpl::empty<typename boost::mpl::pop_front<Types>::type>::value >
-// {
-//     template<typename T> std::pair<typename std::vector<T>::iterator      ,typename std::vector<T>::iterator      > get()       { return get_impl( type2type<T>() ); }
-//     template<typename T> std::pair<typename std::vector<T>::const_iterator,typename std::vector<T>::const_iterator> get() const { return get_impl( type2type<T>() ); }
-
-//     template<typename T> void add(T const & val) { return add_impl( val ); }
-
-//     template<typename T> size_t size() { return size_impl( type2type<T>() ); }
-// };
 
 
 
--- src/util/mpl/TypesHolders_test.cc
+++ src/util/mpl/TypesHolders_test.cc
@@ -2,9 +2,13 @@
 #include <util/mpl/TypesHolders.hh>
 #include <boost/mpl/vector.hpp>
 #include <boost/mpl/list.hpp>
+#include <boost/mpl/set.hpp>
+#include <boost/mpl/begin.hpp>
+#include <boost/mpl/end.hpp>
+#include <boost/mpl/iterator_range.hpp>
+#include <boost/mpl/for_each.hpp>
 #include <vector>
 #include <list>
-#include <set>
 #include <boost/foreach.hpp>
 
 namespace scheme {
@@ -15,9 +19,53 @@ using std::cout;
 using std::endl;
 using std::vector;
 
+struct PrintType {
+    template<typename T>
+    void operator()(T const & t){
+        cout << typeid(t).name() << endl;
+    }
+};
 
-TEST(TypesValues,holds_multiple_types_mplvector){
+TEST(TypesValues,holds_multiple_types_mpl_vector){
     TypesValues<boost::mpl::vector<double,int,char> > holder;
+    holder.get<double>() = 1.0;
+    holder.get<int>() = 2;  
+    holder.get<char>() = 'A';
+    ASSERT_EQ( holder.get<double>() , 1.0 );
+    ASSERT_EQ( holder.get<int>()    , 2   );
+    ASSERT_EQ( holder.get<char>()   , 'A' );
+    holder.get<double>() = 1.23456789;
+    holder.get<int>()    = 7;
+    holder.get<char>()   = 'B';
+    ASSERT_EQ( holder.get<double>() , 1.23456789 );
+    ASSERT_EQ( holder.get<int>()    , 7   );
+    ASSERT_EQ( holder.get<char>()   , 'B' );
+
+    // boost::mpl::for_each<boost::mpl::vector<double,int,char> >( PrintType() );
+}
+
+TEST(TypesValues,holds_multiple_types_mpl_set){
+    typedef boost::mpl::set<double,int,char> Types;
+    TypesValues<Types> holder;
+    holder.get<double>() = 1.0;
+    holder.get<int>() = 2;  
+    holder.get<char>() = 'A';
+    ASSERT_EQ( holder.get<double>() , 1.0 );
+    ASSERT_EQ( holder.get<int>()    , 2   );
+    ASSERT_EQ( holder.get<char>()   , 'A' );
+    holder.get<double>() = 1.23456789;
+    holder.get<int>()    = 7;
+    holder.get<char>()   = 'B';
+    ASSERT_EQ( holder.get<double>() , 1.23456789 );
+    ASSERT_EQ( holder.get<int>()    , 7   );
+    ASSERT_EQ( holder.get<char>()   , 'B' );
+}
+TEST(TypesValues,holds_multiple_types_mpl_veiw){
+    typedef boost::mpl::set<double,int,char> Types;
+    typedef boost::mpl::iterator_range<
+        boost::mpl::begin<Types>::type,
+        boost::mpl::end  <Types>::type > View;
+    TypesValues<View> holder;
     holder.get<double>() = 1.0;
     holder.get<int>() = 2;  
     holder.get<char>() = 'A';
@@ -119,73 +167,6 @@ TEST(TypesContainers,holds_lists){
     ASSERT_EQ( *(++holder.get<char  >().begin()) , 'B' );
 }
 
-TEST(TypesContainers,holds_sets){
-    TypesContainersComp<
-        boost::mpl::vector<
-            double,
-            int,
-            char
-        >, std::set > holder;
-    holder.get<double>().insert(1.0);
-    holder.get<int   >().insert( 2 );  
-    holder.get<char  >().insert('A');
-    ASSERT_EQ( holder.get<double>().size() , 1 );
-    ASSERT_EQ( holder.get<int   >().size() , 1 );
-    ASSERT_EQ( holder.get<char  >().size() , 1 );
-    ASSERT_EQ( *(holder.get<double>().begin()) , 1.0 );
-    ASSERT_EQ( *(holder.get<int   >().begin()) ,  2  );
-    ASSERT_EQ( *(holder.get<char  >().begin()) , 'A' );
-    holder.get<double>().insert(2.0);
-    holder.get<int   >().insert( 6 );  
-    holder.get<char  >().insert('B');
-    ASSERT_EQ( holder.get<double>().size() , 2 );
-    ASSERT_EQ( holder.get<int   >().size() , 2 );
-    ASSERT_EQ( holder.get<char  >().size() , 2 );
-    ASSERT_EQ( *(holder.get<double>().begin()) , 1.0 );
-    ASSERT_EQ( *(holder.get<int   >().begin()) ,  2  );
-    ASSERT_EQ( *(holder.get<char  >().begin()) , 'A' );
-    ASSERT_EQ( *(++holder.get<double>().begin()) , 2.0 );
-    ASSERT_EQ( *(++holder.get<int   >().begin()) ,  6  );
-    ASSERT_EQ( *(++holder.get<char  >().begin()) , 'B' );
-}
-
-// TEST(TypesContainer,holds_multiple_types){
-//     TypesVecS<
-//         boost::mpl::list<
-//             double,
-//             int,
-//             char
-//         > > holder;
-//     ASSERT_EQ( holder.size<double>() , 0 );
-//     ASSERT_EQ( holder.size<int   >() , 0 );
-//     ASSERT_EQ( holder.size<char  >() , 0 );
-//     holder.add<double>(1.0);
-//     holder.add<int   >( 2 );  
-//     holder.add<char  >('A');
-//     ASSERT_EQ( holder.size<double>() , 1 );
-//     ASSERT_EQ( holder.size<int   >() , 1 );
-//     ASSERT_EQ( holder.size<char  >() , 1 );
-//     std::pair<std::vector<double>::iterator,std::vector<double>::iterator> ip = holder.get<double>();
-//     BOOST_FOREACH( double const & d, holder.get<double>() ){
-//         cout << d << endl;
-//     }
-//     // ASSERT_EQ( holder.get<vector<double> >()[0] , 1.0 );
-//     // ASSERT_EQ( holder.get<vector<int   > >()[0] ,  2  );
-//     // ASSERT_EQ( holder.get<vector<char  > >()[0] , 'A' );
-//     holder.add<double>(2.0);
-//     holder.add<int   >( 6 );  
-//     holder.add<char  >('B');
-//     ASSERT_EQ( holder.size<double>() , 2 );
-//     ASSERT_EQ( holder.size<int   >() , 2 );
-//     ASSERT_EQ( holder.size<char  >() , 2 );
-//     // ASSERT_EQ( holder.get<vector<double> >()[0] , 1.0 );
-//     // ASSERT_EQ( holder.get<vector<int   > >()[0] ,  2  );
-//     // ASSERT_EQ( holder.get<vector<char  > >()[0] , 'A' );
-//     // ASSERT_EQ( holder.get<vector<double> >()[1] , 2.0 );
-//     // ASSERT_EQ( holder.get<vector<int   > >()[1] ,  6  );
-//     // ASSERT_EQ( holder.get<vector<char  > >()[1] , 'B' );
-
-// }
 
 }
 }
