5c0996bb39e5a6de750809c0c70b4811873e939c
William Sheffler <will@sheffler.me>
2016-05-05 12:07:27 -0700

    build system omp fixes

--- apps/rosetta/python/test/test_pybind.cc
+++ apps/rosetta/python/test/test_pybind.cc
@@ -18,7 +18,7 @@ std::shared_ptr<Foo> make_Foo(){
 PYBIND11_DECLARE_HOLDER_TYPE(T, std::shared_ptr<T>);
 
 PYBIND11_PLUGIN(test_pybind) {
-    py::module m("test_pybind", "test_pybind");
+    py::module m("test_pybind", "test pybind11");
 
 	py::class_< Foo, std::shared_ptr<Foo> >(m, "Foo")
 		.def( py::init<>() )
