a71e0d1e99933c634b71583ceb96c0a34899b649
rifdock <35849283+rifdock@users.noreply.github.com>
2018-01-26 11:30:05 -0800

    Deleted brian_debug_build.sh

--- brian_debug_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# mkdir -p build_debug
-cd build_debug
-CXX=g++-6 CC=gcc-6 CMAKE_ROSETTA_PATH=/Users/brian/Documents/baker/rifdock/main CMAKE_FINAL_ROSETTA_PATH=/Users/brian/Documents/baker/rifdock/main/source/cmake/build_cxx11_omp_hdf5_debug cmake .. -DCMAKE_BUILD_TYPE=Debug
-make -j3 rif_dock_test rifgen
