78438333979c4d80e5786c05f8879ef5de60ba38
bcov77 <bcov@uw.edu>
2018-05-22 11:59:05 -0700

    Forgot to add SasaTasks*

---
+++ apps/rosetta/riflib/rifdock_tasks/SasaTasks.hh
@@ -0,0 +1,143 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://wsic_dockosettacommons.org. Questions about this casic_dock
+// (c) addressed to University of Waprotocolsgton UW TechTransfer, email: license@u.washington.eprotocols
+
+#ifndef INCLUDED_riflib_rifdock_tasks_SasaTasks_hh
+#define INCLUDED_riflib_rifdock_tasks_SasaTasks_hh
+
+#include <riflib/types.hh>
+#include <riflib/task/AnyPointTask.hh>
+
+#include <string>
+#include <vector>
+
+
+
+namespace devel {
+namespace scheme {
+
+struct FilterBySasaTask : public AnyPointTask {
+
+    FilterBySasaTask(
+        float sasa_cut
+        ) :
+        sasa_cut_( sasa_cut )
+        {}
+
+    shared_ptr<std::vector<SearchPoint>> 
+    return_search_points( 
+        shared_ptr<std::vector<SearchPoint>> search_points, 
+        RifDockData & rdd, 
+        ProtocolData & pd ) override;
+
+    shared_ptr<std::vector<SearchPointWithRots>> 
+    return_search_point_with_rotss( 
+        shared_ptr<std::vector<SearchPointWithRots>> search_point_with_rotss, 
+        RifDockData & rdd, 
+        ProtocolData & pd ) override;
+
+    shared_ptr<std::vector<RifDockResult>> 
+    return_rif_dock_results( 
+        shared_ptr<std::vector<RifDockResult>> rif_dock_results, 
+        RifDockData & rdd, 
+        ProtocolData & pd ) override;
+
+private:
+    template<class AnyPoint>
+    shared_ptr<std::vector<AnyPoint>>
+    return_any_points( 
+        shared_ptr<std::vector<AnyPoint>> any_points, 
+        RifDockData & rdd, 
+        ProtocolData & pd ); // override
+
+private:
+    float sasa_cut_;
+
+};
+
+
+struct FilterByScorePer1000SasaTask : public AnyPointTask {
+
+    FilterByScorePer1000SasaTask(
+        float score_per_1000_sasa_cut
+        ) :
+        score_per_1000_sasa_cut_( score_per_1000_sasa_cut )
+        {}
+
+    shared_ptr<std::vector<SearchPoint>> 
+    return_search_points( 
+        shared_ptr<std::vector<SearchPoint>> search_points, 
+        RifDockData & rdd, 
+        ProtocolData & pd ) override;
+
+    shared_ptr<std::vector<SearchPointWithRots>> 
+    return_search_point_with_rotss( 
+        shared_ptr<std::vector<SearchPointWithRots>> search_point_with_rotss, 
+        RifDockData & rdd, 
+        ProtocolData & pd ) override;
+
+    shared_ptr<std::vector<RifDockResult>> 
+    return_rif_dock_results( 
+        shared_ptr<std::vector<RifDockResult>> rif_dock_results, 
+        RifDockData & rdd, 
+        ProtocolData & pd ) override;
+
+private:
+    template<class AnyPoint>
+    shared_ptr<std::vector<AnyPoint>>
+    return_any_points( 
+        shared_ptr<std::vector<AnyPoint>> any_points, 
+        RifDockData & rdd, 
+        ProtocolData & pd ); // override
+
+private:
+    float score_per_1000_sasa_cut_;
+
+};
+
+struct SortByScorePer1000SasaTask : public AnyPointTask {
+
+    SortByScorePer1000SasaTask(
+        ) 
+        {}
+
+    shared_ptr<std::vector<SearchPoint>> 
+    return_search_points( 
+        shared_ptr<std::vector<SearchPoint>> search_points, 
+        RifDockData & rdd, 
+        ProtocolData & pd ) override;
+
+    shared_ptr<std::vector<SearchPointWithRots>> 
+    return_search_point_with_rotss( 
+        shared_ptr<std::vector<SearchPointWithRots>> search_point_with_rotss, 
+        RifDockData & rdd, 
+        ProtocolData & pd ) override;
+
+    shared_ptr<std::vector<RifDockResult>> 
+    return_rif_dock_results( 
+        shared_ptr<std::vector<RifDockResult>> rif_dock_results, 
+        RifDockData & rdd, 
+        ProtocolData & pd ) override;
+
+private:
+    template<class AnyPoint>
+    shared_ptr<std::vector<AnyPoint>>
+    return_any_points( 
+        shared_ptr<std::vector<AnyPoint>> any_points, 
+        RifDockData & rdd, 
+        ProtocolData & pd ); // override
+
+private:
+
+};
+
+
+
+}}
+
+#endif
